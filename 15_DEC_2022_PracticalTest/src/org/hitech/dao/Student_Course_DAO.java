7541b6dff46612529aea1572324344e9252f9c51
Dell <Dell@DESKTOP-FKG1LJV>
2022-12-15 23:17:48 +0530

    Version4

---
+++ 15_DEC_2022_PracticalTest/src/org/hitech/dao/Student_Course_DAO.java
@@ -0,0 +1,18 @@
+package org.hitech.dao;
+import org.hitech.model.*;
+import java.util.*;
+import org.hitech.service.*;
+import org.hitech.view.*;
+public class Student_Course_DAO {
+	public static HashMap<Student,Course> map=new HashMap<Student,Course>();
+	public void addStudentCourseDetails(Student s,Course c) {
+		map.put(s,c);
+	}
+	public HashMap<Student, Course>getStudentCourseDetails(){
+		return map;
+	}
+	public void deleteStudentCourseDetails(Student s) {
+		map.remove(s);
+	}
+	 
+}
